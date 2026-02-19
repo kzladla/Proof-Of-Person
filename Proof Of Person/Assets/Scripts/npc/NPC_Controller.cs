a17ef4589579f3d3e409dbdcafb85e01b704bae4
kzladla <kzlalford029@gmail.com>
2026-02-19 18:03:32 +0000

    queue

--- Proof Of Person/Assets/Scripts/npc/NPC_Controller.cs
+++ Proof Of Person/Assets/Scripts/npc/NPC_Controller.cs
@@ -3,13 +3,40 @@ using UnityEngine.AI;
 
 public class NPC_Controller : MonoBehaviour
 {
-    public Transform Target;
-    public NavMeshAgent Agent;
-   
+    public NavMeshAgent agent;
+    public Transform[] targets;   // Assign in Inspector
 
-    // Update is called once per frame
+    [SerializeField] private int currentTargetIndex = 0;
+
+    void Start()
+    {
+        if (agent == null)
+            agent = GetComponent<NavMeshAgent>();
+
+        if (targets.Length > 0)
+            agent.SetDestination(targets[currentTargetIndex].position);
+    }
+
     void Update()
     {
-        Agent.SetDestination(Target.position);
+        // Press E to switch target
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SwitchTarget();
+        }
+    }
+
+    void SwitchTarget()
+    {
+        if (targets.Length == 0) return;
+
+        // Go to next target
+        currentTargetIndex++;
+
+        // Loop back to start if we reach the end
+        if (currentTargetIndex >= targets.Length)
+            currentTargetIndex = 0;
+
+        agent.SetDestination(targets[currentTargetIndex].position);
     }
 }
