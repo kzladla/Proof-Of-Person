ea661e9b9db93dfbb4610b37420bb5483ded64b0
kzladla <kzlalford029@gmail.com>
2026-02-24 21:30:13 +0000

    j

--- Proof Of Person/Assets/Scripts/npc/Targets.cs
+++ Proof Of Person/Assets/Scripts/npc/Targets.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class Targets : MonoBehaviour
 {
-    public bool npcInside = false;
-
-    private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("NPC"))
         {
-            npcInside = true;
             Destroy(other.gameObject);
-            npcInside = false;
+            Debug.Log("NPC object destroyed");
         }
     }
 
