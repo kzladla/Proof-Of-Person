84f2f30023701f455fdacc26b67577f1228deab3
kzladla <kzlalford029@gmail.com>
2026-02-25 09:30:05 +0000

    prototype 1

--- Proof Of Person/Assets/Scripts/npc/NPC_Controller.cs
+++ Proof Of Person/Assets/Scripts/npc/NPC_Controller.cs
@@ -4,28 +4,33 @@ using UnityEngine.AI;
 
 public class NPC_Controller : MonoBehaviour
 {
-    public NavMeshAgent agent;
-    public Transform[] targets;   // Assign in Inspector
 
+    public NavMeshAgent agent;
+    public Transform[] targets;   // array of targets to move position forward
+
+    // tagets for approve and deny
     [SerializeField] private Transform Approve_Target;
     [SerializeField] private Transform Deny_Target;
 
+    // current position in queue 0 = back - 3 = front
     public int currentTargetIndex = 0;
+
 
     public GameObject uiCanvas;
 
     void Start()
-    {
+    {   // get navmesh agent component
         if (agent == null)
             agent = GetComponent<NavMeshAgent>();
 
+    // set first target to move to
         if (targets.Length > 0)
             agent.SetDestination(targets[currentTargetIndex].position);
     }
 
     void Update()
     {
-        // press e to switch target
+        // press e to switch target / move queue forward
         if (Input.GetKeyDown(KeyCode.E))
         {
             SwitchTarget();
@@ -34,6 +39,7 @@ public class NPC_Controller : MonoBehaviour
 
     void SwitchTarget()
     {
+        // if no targets, do nothing
         if (targets.Length == 0) return;
 
         // go to next queue target
@@ -48,6 +54,7 @@ public class NPC_Controller : MonoBehaviour
 
     public void showUI()
     {
+        // show UI canvas and unlock cursor
         uiCanvas.SetActive(true);
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
@@ -55,20 +62,19 @@ public class NPC_Controller : MonoBehaviour
 
     public void hideUI()
     {
+        // hide UI canvas and lock cursor
         uiCanvas.SetActive(false);
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
     public void Approve()
-    {
+    {   // move to approve target position
         agent.SetDestination(Approve_Target.position);
-        Debug.Log("Approved ");
         hideUI();
     }
     public void Deny()
-    {
+    {   // move to deny target position
         agent.SetDestination(Deny_Target.position);
-        Debug.Log("Denied ");
         hideUI();
     }
 }
--- Proof Of Person/Assets/Scripts/player/Interactor.cs
+++ Proof Of Person/Assets/Scripts/player/Interactor.cs
@@ -14,19 +14,20 @@ public class Interactor : MonoBehaviour
 
     void Update()
     {
+        // create raycast from interactor source forward
         Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
-
+        // check if ray hits an interactable object within range
         if (Physics.Raycast(r, out RaycastHit hitInfo, InteractRange))
-        {
+        {   // if it does, check if it has an NPC_Controller script attached
             if (hitInfo.collider.TryGetComponent(out NPC_Controller npc))
-            {
+            {   // if a is pressed send to approve target from controller script
                 if (Input.GetKeyDown(KeyCode.A))
-                {
+                {   // 3 is the front of the queue, only approve if at the front
                     if (npc.currentTargetIndex == 3)
                     {
                         npc.Approve();
                     }
-                }
+                }// if d is pressed send to deny target from controller script
                 if (Input.GetKeyDown(KeyCode.D))
                 {
                     if (npc.currentTargetIndex == 3)
@@ -36,7 +37,7 @@ public class Interactor : MonoBehaviour
                 }
             }
         }
-
+        // show ray in scene view for debugging
         Debug.DrawRay(InteractorSource.position, InteractorSource.forward * InteractRange, Color.red);
     }
 
